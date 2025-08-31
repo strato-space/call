74b257f68b55d658619b55027f85170c40e077c3
Валерий Павлович <s@iqdoctor.pro>
2025-08-31 07:04:01 +0300

    comment gsh

--- app/call.py
+++ app/call.py
@@ -1071,14 +1071,16 @@ async def run_digest_pipeline(samples_dir: str, agent_path: str = None, user_inp
         },
         name="seq",
         client_session_timeout_seconds=30
-    ) as server_seq, MCPServerStdioHook(
-            params={
-                "command": "uvx",
-                "args": ["--env-file", samples_dir + "/server/mcp/.env", "mcp-google-sheets@latest"]
-            },
-            name="gsh",
-        client_session_timeout_seconds=30
-    ) as server_gsheets, MCPServerStdioHook(
+    ) as server_seq, 
+    # MCPServerStdioHook(
+    #         params={
+    #             "command": "uvx",
+    #             "args": ["--env-file", samples_dir + "/server/mcp/.env", "mcp-google-sheets@latest"]
+    #         },
+    #         name="gsh",
+    #     client_session_timeout_seconds=30
+    # ) as server_gsheets, 
+    MCPServerStdioHook(
             params={
                 "command": "uvx",
                 "args": [
